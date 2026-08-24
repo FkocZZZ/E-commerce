7a8bcf3fa4ac949bc95dd6aecef412762e6fa8c6
Fkoc <vinhtuan18@gmail.com>
2026-08-24 23:37:33 +0700

    feat: complete configuration for config and typeorm database connection

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST') || '127.0.0.1',
+        port: +configService.get('DB_PORT') || 3306,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        synchronize: true,
+        autoLoadEntities: true,
+        // dropSchema: true
+
+      })
+    }),
+    UsersModule,
+    CategoriesModule,
+    ProductsModule,
+    CouponsModule,
+    OrdersModule,
+    PaymentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
